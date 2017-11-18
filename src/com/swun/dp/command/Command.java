b0dbabdfd3ca09b33d6f6e9e80d91171534914ea
meifan <meifan_mvc@foxmail.com>
2017-11-18 16:44:42 +0800

    update

--- src/com/swun/dp/command/Command.java
+++ src/com/swun/dp/command/Command.java
@@ -2,4 +2,5 @@ package com.swun.dp.command;
 
 public interface Command {
     public void execute();
+    public void undo();
 }
---
+++ src/com/swun/dp/command/Stereo.java
@@ -0,0 +1,13 @@
+package com.swun.dp.command;
+
+public class Stereo {
+    public void on(){
+        System.out.println("---the stereo  on -------");
+    }
+
+    public void off(){
+        System.out.println("---the stereo  off -------");
+    }
+
+
+}
